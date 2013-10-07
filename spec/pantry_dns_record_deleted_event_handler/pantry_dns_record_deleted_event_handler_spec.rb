4d9ef47e13b84e65aba268f8877e125dd44e198d
Kieran Manning <kieran.manning@wonga.com>
2013-10-07 17:50:30 +0100

    basic functionality added

--- spec/pantry_dns_record_deleted_event_handler/pantry_dns_record_deleted_event_handler_spec.rb
+++ spec/pantry_dns_record_deleted_event_handler/pantry_dns_record_deleted_event_handler_spec.rb
@@ -2,9 +2,20 @@ require 'spec_helper'
 require_relative '../../pantry_dns_record_deleted_event_handler/pantry_dns_record_deleted_event_handler'
 
 describe Wonga::Daemon::PantryDnsRecordDeletedEventHandler do
+  let(:config) {
+    {
+      "pantry" => {
+        "api_key" => "some_api_key",
+        "request_timeout" => 10,
+        "url" => "http://some.url"
+      }
+    }
+  }
+  let(:logger) { instance_double('Logger').as_null_object }
+
+  subject { Wonga::Daemon::PantryDnsRecordDeletedEventHandler.new(config, logger) }
+
   it_behaves_like 'handler'
-  describe "#handle_message" do
-    #add in unit test code for handle message here.
-  end
+
 end
 
